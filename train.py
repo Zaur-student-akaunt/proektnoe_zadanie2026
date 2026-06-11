de3f007207fc9f07b14699c1f4618a9109bb8cea
Zaur-student-akaunt <nasrullaevzaur3@gmail.com>
2026-06-11 23:57:18 +0300

    Add files via upload

---
+++ train.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+def main():
+    model = YOLO("yolov8n.pt")
+
+    model.train(
+        data="dataset/data.yaml",
+        epochs=50,
+        imgsz=640,
+        batch=16,
+        name="garbage_detector"
+    )
+
+if __name__ == "__main__":
+    main()
